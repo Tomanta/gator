b6362166711a93927dfa88472072efe8f5259b1a
Tomanta <bzednick@gmail.com>
2025-04-07 19:39:49 -0500

    Added some structure for cli

--- main.go
+++ main.go
@@ -7,6 +7,30 @@ import (
 	"github.com/tomanta/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+
+}
+
+func (c *commands) run(name string, f func(*state, command) error) {
+
+}
+
+func handlerLogin(s *state, cmd command) error {
+	// If arg's slice is empty, return an error
+	// Use state to access te config struct to set the user to the username, return any errors
+	// Print a message to the terminal that the user has been set
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
