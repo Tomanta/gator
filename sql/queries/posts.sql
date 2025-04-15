71ae8214312a982a0de7a4bcf8a0e28337ad7c72
Tomanta <bzednick@gmail.com>
2025-04-15 17:33:02 -0500

    Added queries to insert and query posts

---
+++ sql/queries/posts.sql
@@ -0,0 +1,22 @@
+-- name: CreatePost :one
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+)
+RETURNING *;
+
+-- name: GetPosts :many
+SELECT posts.id, posts.created_at, posts.updated_at, posts.title, posts.url, posts.description, posts.published_at
+FROM feeds
+INNER JOIN feed_follows ff ON ff.feed_id = feeds.feed_id
+INNER JOIN posts ON posts.feed_id = feeds.feed_id
+WHERE ff.user_id = $1
+ORDER BY published_at DESC
+LIMIT $2;
